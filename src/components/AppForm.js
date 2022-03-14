c23f5613068990ac83765a038394f6fb5a61843b
Shruti Vaidya <vaidya.shruti1108@gmail.com>
2022-03-14 18:57:22 +0545

    added local storage

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import { Form, Row, Col, Button } from 'react-bootstrap'
 
+//getting the values from localstorage//
+    const getDatafromLS=()=>{
+        const data = localStorage.getItem('records');
+        if(data){
+            return JSON.parse(data);
+        }
+        else{
+            return []
+        }
+    }
 
 const AppForm = ({handleClose}) => {
 
     //main array of objects
-    const [records, setRecords] = useState([]);
+    const [records, setRecords] = useState(getDatafromLS());
 
     //input field states //
 
     const [name, setName] = useState('');
     const [contact, setContact] = useState('');
     const [address, setAddress] = useState('');
-    const [profilepic, setProfilepic] = useState('');
+    const [profilepic, setProfilepic] = useState(null);
 
     //form submit event//
     const handleSubmit=(e)=>{
@@ -36,9 +46,9 @@ const AppForm = ({handleClose}) => {
 
     
     //saving data to local storage//
-   // useEffect(()=>{
-      //  localStorage.setItem('records', JSON.stringify(records));
-    //},[records])
+    useEffect(()=>{
+       localStorage.setItem('records', JSON.stringify(records));
+    },[records])
    
   return (
     <div>
@@ -47,35 +57,46 @@ const AppForm = ({handleClose}) => {
                 <Col>
                     <Form.Label>Employee Name</Form.Label>
                     <Form.Control 
+                        type='text'
                         placeholder="Name"
                         onChange={(e) => setName(e.target.value)}
                         value={name}
                     />
                 </Col>
                 <Col>
-                    <Form.Label>Address</Form.Label>
+                <Form.Label>Contact Details</Form.Label>
                     <Form.Control 
-                        placeholder="Address" 
-                        onChange={(e) => setAddress(e.target.value)}
-                        value={address} 
+                        type='number'
+                        placeholder="012345"
+                        onChange={(e) => setContact(e.target.value)}
+                        value={contact}  
                     />
                 </Col>
             </Row>
                 <br/>
             <Row>
                 <Col>
-                    <Form.Label>Contact Details</Form.Label>
+                   
+                     <Form.Label>Address</Form.Label>
                     <Form.Control 
-                        placeholder="012345"
-                        onChange={(e) => setContact(e.target.value)}
-                        value={contact}  
+                        placeholder="Address" 
+                        onChange={(e) => setAddress(e.target.value)}
+                        value={address} 
                     />
                 </Col>
-                <Col>
+              
+            </Row>
+            <br/>
+            <Row>
+            <Col>
                     <Form.Label>Upload Pic</Form.Label>
-                    <Form.Control 
-                        onChange={(e) => setProfilepic(e.target.value)}
-                        value={profilepic} 
+                    <Form.Control
+                        type='file' 
+                        name="myImage"
+                        onChange={(e) => {
+                            console.log(e.target.files[0]);
+                            setProfilepic(e.target.files[0])
+                        }} 
                     />
                 </Col>
             </Row>
